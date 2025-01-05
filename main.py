49cdf00e2eff5c1c979faa8c541b995708015893
kshitijrajsharma <skshitizraj@gmail.com>
2025-01-05 16:17:35 +0100

    Refactor tile processing logic into TileProcessor class and update image generation with customizable colormap

--- main.py
+++ main.py
@@ -6,26 +6,20 @@ import shutil
 import sys
 import time
 from datetime import datetime, timedelta
-from io import BytesIO
 
 import httpx
 import matplotlib
-import mercantile
-import numpy as np
-from aiocache import cached
 from fastapi import BackgroundTasks, FastAPI, HTTPException, Query, Response
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from matplotlib import pyplot as plt
-from PIL import Image
-from rio_tiler.io import COGReader
 from shapely.geometry import box, mapping
 from starlette.requests import Request
 from starlette.status import HTTP_504_GATEWAY_TIMEOUT
 
 from src.vcube.engine import VCubeProcessor
+from src.vcube.tile import TileProcessor
 
 app = FastAPI()
 
@@ -66,7 +60,7 @@ async def read_index(request: Request):
 
 
 @app.get("/about", response_class=HTMLResponse)
-async def read_index(request: Request):
+async def read_about(request: Request):
     return templates.TemplateResponse("about.html", {"request": request})
 
 
@@ -119,15 +113,6 @@ async def get_sentinel2_bands(
             raise HTTPException(status_code=404, detail="Band not found")
     else:
         return {key: value["title"] for key, value in sentinel2_assets.items()}
-
-
-async def fetch_tile(url, x, y, z):
-    def read_tile():
-        with COGReader(url) as cog:
-            tile, _ = cog.tile(x, y, z)
-            return tile
-
-    return await asyncio.to_thread(read_tile)
 
 
 @app.get("/export")
@@ -287,86 +272,6 @@ async def search_images(
     return results
 
 
-@cached(ttl=60 * 1)
-async def cached_generate_tile(
-    x: int,
-    y: int,
-    z: int,
-    start_date: str,
-    end_date: str,
-    cloud_cover: int,
-    band1: str,
-    band2: str,
-    formula: str,
-) -> bytes:
-    tile = mercantile.Tile(x, y, z)
-    bbox = mercantile.bounds(tile)
-    bbox_polygon = box(bbox.west, bbox.south, bbox.east, bbox.north)
-    bbox_geojson = mapping(bbox_polygon)
-    STAC_API_URL = "https://earth-search.aws.element84.com/v1/search"
-    search_params = {
-        "collections": ["sentinel-2-l2a"],
-        "datetime": f"{start_date}T00:00:00Z/{end_date}T23:59:59Z",
-        "query": {"eo:cloud_cover": {"lt": cloud_cover}},
-        "intersects": bbox_geojson,
-        "limit": 1,
-    }
-
-    async with httpx.AsyncClient() as client:
-        response = await client.post(STAC_API_URL, json=search_params)
-    if response.status_code != 200:
-        raise HTTPException(status_code=404, detail="Error searching STAC API")
-
-    results = response.json()
-    if not results["features"]:
-        raise HTTPException(
-            status_code=404, detail="No images found for the given parameters"
-        )
-
-    feature = results["features"][0]
-    band1_url = feature["assets"][band1]["href"]
-    band2_url = feature["assets"][band2]["href"] if band2 else None
-
-    try:
-        tasks = [fetch_tile(band1_url, x, y, z)]
-        if band2_url:
-            tasks.append(fetch_tile(band2_url, x, y, z))
-
-        tiles = await asyncio.gather(*tasks)
-        band1 = tiles[0]
-        band2 = tiles[1] if band2_url else None
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-    if band2 is not None:
-        # Consider this is single band
-        # Perform custom calculation with two bands
-        band1 = band1[0].astype(float)
-        band2 = band2[0].astype(float)
-
-        result = eval(formula)
-        image = apply_colormap(result)
-    else:
-        inner_bands = band1.shape[0]
-        if inner_bands == 1:
-            # Single band image
-            band1 = band1[0].astype(float)
-            result = eval(formula)
-            image = apply_colormap(result)
-        else:
-            # Multi band image
-            band1 = band1.transpose(
-                1, 2, 0
-            )  # Transpose to (256, 256, 3) or (256, 256, 2)
-            image = Image.fromarray(band1)
-
-    buffered = BytesIO()
-    image.save(buffered, format="PNG")
-    image_bytes = buffered.getvalue()
-
-    return image_bytes, feature
-
-
 @app.get("/tile/{z}/{x}/{y}")
 async def get_tile(
     z: int,
@@ -385,6 +290,7 @@ async def get_tile(
         "band1",
         description="Formula for custom band calculation (example: (band2 - band1) / (band2 + band1) for NDVI)",
     ),
+    colormap_str: str = Query("RdYlGn", description="Colormap for the output image"),
 ):
     if z < 10 or z > 16:
         return JSONResponse(
@@ -403,8 +309,18 @@ async def get_tile(
 
     try:
         start_time = time.time()
-        image_bytes, feature = await cached_generate_tile(
-            x, y, z, start_date, end_date, cloud_cover, band1, band2, formula
+        tile_processor = TileProcessor()
+        image_bytes, feature = await tile_processor.cached_generate_tile(
+            x,
+            y,
+            z,
+            start_date,
+            end_date,
+            cloud_cover,
+            band1,
+            band2,
+            formula,
+            colormap_str,
         )
         computation_time = time.time() - start_time
 
@@ -417,11 +333,3 @@ async def get_tile(
         return Response(content=image_bytes, media_type="image/png", headers=headers)
     except Exception as ex:
         return JSONResponse(content={"error": "Computation Error"}, status_code=504)
-
-
-def apply_colormap(result):
-    result_normalized = (result - result.min()) / (result.max() - result.min())
-    colormap = plt.get_cmap("RdYlGn")
-    result_colored = colormap(result_normalized)
-    result_image = (result_colored[:, :, :3] * 255).astype(np.uint8)
-    return Image.fromarray(result_image)
